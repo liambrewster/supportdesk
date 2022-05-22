238f2749288ede9960ab60a78fb0afbfb8dfcca3
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-05-23 00:08:07 +0100

    feat: added the database connection config

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,15 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
+const colors = require('colors')
 const {errorHandler} =require('./middleware/errorMiddleware')
-
-
-
+const connectDB = require('./config/db')
 const PORT = process.env.PORT || 3000
+
+//connect to database
+connectDB()
+
 const app = express()
+
 app.use(express.json())
 app.use(express.urlencoded({extended:false}))
 

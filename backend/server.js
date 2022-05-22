52a0b13e58bd0c16cb9cd67df13361de7770cd64
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-05-22 23:30:16 +0100

    feat: setup express routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 3000
 const app = express()
 
 app.get('/', (req,res) => {
     res.status(200).json({message:"Welcome to the Support Desk API √√√",status:'Alive'})
 })
+
+//Routes
+app.use('/api/users', require('./routes/userRoutes'))
 
 app.listen(PORT, ()=> console.log(`√√√ Server Started on Port ${PORT} √√√`))

ac222b6a97effdd6d176cf140671ce2ab6ffcdf8
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-05-27 07:23:43 +0100

    feat: added registration function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,6 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 
 
 // @desc        Register a User
@@ -11,6 +13,35 @@ const registerUser = asyncHandler(async(req,res) => {
     if(!name || !email || !password) {
         res.status(400)
         throw new Error('Please Fill All Fields With Data')
+    }
+    // user exists?
+    const userExists = await User.findOne({email})
+    if(userExists){
+        res.status(400)
+        throw new Error('User Already Exists')
+    }
+
+    // hash password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password,salt)
+
+    // create the user
+    const user = await User.create({
+        name,
+        email,
+        password:hashedPassword,
+    })
+
+    if(user) {
+        res.status(201).json({
+           _id:user._id,
+           name:user.name,
+           email:user.email
+
+        })
+    } else {
+        res.status(400)
+        throw new error('invalid user data')
     }
 
 

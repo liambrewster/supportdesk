d0d7c52edf91bb8ab3410ce81eacdfaaaaac354e
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-06-06 21:39:33 +0100

    feat: hook forms up to  redux

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,6 +1,8 @@
 import {useState,} from 'react'
 import {FaUser} from 'react-icons/fa'
 import {toast} from 'react-toastify'
+import {useSelector, useDispatch} from 'react-redux'
+import {register} from '../features/auth/authSlice'
 
 function Register() {
 
@@ -12,6 +14,8 @@ function Register() {
   })
 
   const {name,email,password,password2} = formData
+  const dispatch = useDispatch()
+  const {user,isLoading,isSuccess,message} = useSelector(state => state.auth)
 
   const onChange = (e) => {
     setFormData((prevState)=>({
@@ -24,6 +28,14 @@ function Register() {
     e.preventDefault()
     if(password !== password2){
       toast.error('passwords do not match')
+    } else {
+      const userData = {
+        name,
+        email,
+        password
+      }
+      dispatch(register(userData))
+
     }
   }
 
@@ -31,7 +43,7 @@ function Register() {
     <>
     <section className='heading'>
       <h1>
-        <FaUser/> Register
+        <FaUser/> Register {user}
       </h1>
       <p>Please create an account</p>
 

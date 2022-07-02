2d66e2930b091152db686a3ea14d03574009cae8
Liam Brewster <liam.brewster@hotmail.co.uk>
2022-07-02 22:32:49 +0100

    feat: setup the redux for notes

---
+++ frontend/src/features/notes/noteService.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const API_URL = 'api/tickets/'
+
+//Get Single Ticket Notes
+const getNotes = async (ticketID, token) => {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    }
+    const response = await axios.get(API_URL+ticketID+'/notes',config)
+    return response.data
+}
+
+const noteService = {
+    getNotes
+}
+
+export default noteService
